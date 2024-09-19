a70c6b2d3c879ccf43602715b011861a8a8f7709
justinavu <justinvu2008@gmail.com>
2024-09-19 10:09:14 -0700

    sync

--- Clock.java
+++ Clock.java
@@ -2,9 +2,26 @@ public class Clock
 {
   public static void main(String[] args)
   {
-    // test code here
+          timeOfDay(0);   
+          timeOfDay(8);   
+          timeOfDay(12);  
+          timeOfDay(14);
+          timeOfDay(19);  
+          timeOfDay(21);  
+      }
+  public static void timeOfDay(int hour) {
+    if (hour == 0) {
+        System.out.println("midnight");
+    } else if (hour == 12) {
+        System.out.println("noon");
+    } else if (hour == 18) {
+        System.out.println("dusk");
+    } else if (hour > 0 && hour < 12) {
+        System.out.println("morning");
+    } else if (hour > 12 && hour < 18) {
+        System.out.println("afternoon");
+    } else if (hour > 18 && hour < 24) {
+        System.out.println("evening");
+    }
   }
-
-  // write code for timeOfDay here
-
 }
--- Coins.java
+++ Coins.java
@@ -1,10 +1,30 @@
 public class Coins
 {
-  public static void main(String[] args)
-  {
-    // test code here
-  }
+  public static void main(String[] args) {
+    coinConverter(1179); 
+    coinConverter(199);    
+    coinConverter(275);  
+    coinConverter(6);     
+}
 
-  // write code for coinConverter
+public static void coinConverter(int pennies) {
+    int dollars = pennies / 100;
+    pennies %= 100; 
 
+    int quarters = pennies / 25;
+    pennies %= 25; 
+
+    int dimes = pennies / 10;
+    pennies %= 10; 
+
+    int nickels = pennies / 5;
+    pennies %= 5; 
+
+    System.out.println("Dollar Bills: " + dollars);
+    System.out.println("Quarters: " + quarters);
+    System.out.println("Dimes: " + dimes);
+    System.out.println("Nickels: " + nickels);
+    System.out.println("Pennies: " + pennies);
+    System.out.println("--------------------------------");
+}
 }
